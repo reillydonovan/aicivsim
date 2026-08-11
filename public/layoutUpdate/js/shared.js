525455ec23aca099eec85316886be727c4623c58
reillydonovan <reillydonovan3@gmail.com>
2026-08-10 21:02:36 -0700

    Bump cache version to 20260810a

--- public/layoutUpdate/js/shared.js
+++ public/layoutUpdate/js/shared.js
@@ -1556,7 +1556,7 @@ var CMD_ITEMS=[
   var NO_WIDGET=['viz.html','explorer.html','xr.html'];
   for(var i=0;i<NO_WIDGET.length;i++){if(path.indexOf(NO_WIDGET[i])!==-1)return}
   var s=document.createElement('script');
-  s.src='js/chat-widget.js?v=20260711b';
+  s.src='js/chat-widget.js?v=20260810a';
   s.defer=true;
   document.body.appendChild(s);
 })();
